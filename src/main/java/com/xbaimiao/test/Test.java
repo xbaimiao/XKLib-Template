4bc68a311a00e2ca2421f4ad188294e02ae8e20e
小白 <67080574+xbaimiao@users.noreply.github.com>
2022-09-09 08:48:08 +0800

    Initial commit

---
+++ src/main/java/com/xbaimiao/test/Test.java
@@ -0,0 +1,13 @@
+package com.xbaimiao.test;
+
+import top.mcplugin.lib.Plugin;
+
+@SuppressWarnings("unused")
+public class Test extends Plugin {
+
+    @Override
+    public void enable() {
+
+    }
+
+}
